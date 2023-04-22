3ae3c877dbdbc762c780335c36172faa0395baaf
Manu Overa <manu.overa@gmail.com>
2023-04-22 11:35:32 +0200

    add mutablesLitPropsController

--- lit/index.d.ts
+++ lit/index.d.ts
@@ -3,8 +3,9 @@
  * Copyright 2021 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
  */
-import '@lit/reactive-element';
-import 'lit-html';
-export * from 'lit-element/lit-element.js';
-export * from 'lit-html/is-server.js';
+import "../@lit/reactive-element/reactive-element";
+import "../lit-html/lit-html";
+export * from "../lit-element/lit-element.js";
+export * from "../lit-html/is-server.js";
+export * from "../lit-extends/mutablesLitPropsController.js";
 //# sourceMappingURL=index.d.ts.map
